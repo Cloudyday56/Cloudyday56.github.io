3cfec6f3e63fbba931283f4f56606780ce09562a
Cloudyday56 <Rchen78900@gmail.com>
2025-08-04 14:04:11 -0400

    Intro component - v1

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,37 @@
-
-
 // Profile pic
 // Background
 
-const Header = () => {
-  return <div>Header</div>;
+const Intro = () => {
+  return (
+    <div className="flex min-h-[60vh] max-w-7xl mx-auto bg-base-800 px-0 py-16 items-center w-full">
+      {/* Bio */}
+      <div className="flex flex-col flex-[7] items-start text-left pl-8 pt-40">
+        <p className="font-montserrat text-6xl text-primary max-w-3xl">
+          Hi! <br />
+          I'm Ru, <br />
+          a SWE student <br />
+          at McGill University. <br />
+          Welcome to my portfolio!
+        </p>
+      </div>
+      {/* Profile Picture and Title */}
+      <div className="flex flex-col flex-[3] pr-8 items-center">
+        <img
+          src="/avatar.png" // change to profile
+          alt="Profile"
+          className="w-77 h-77 object-cover rounded-full shadow-lg border-4 border-primary"
+        />
+        <div className="mt-4 flex flex-col items-center">
+          <span className="text-4xl font-extrabold tracking-wide text-primary drop-shadow-lg">
+            Ru Chen
+          </span>
+          <span className="mt-2 text-xl font-medium tracking-wider text-secondary bg-base-200 bg-opacity-90 px-4 py-2 rounded-full shadow border border-secondary/40 uppercase">
+            Software Engineer
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default Header;
+export default Intro;
