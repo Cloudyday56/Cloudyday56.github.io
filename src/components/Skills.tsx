553123388869112a4c6d245f9595776dc95a08a5
Cloudyday56 <Rchen78900@gmail.com>
2025-08-04 18:28:03 -0400

    Links UI

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,6 +4,11 @@
 // Fields of interest
 // related Skills
 
+// web dev - MERN stack, Next.js, Tailwind CSS
+// game dev - Blender, GameMaker, Godot
+// AI - Python, Scikit-learn, Random Forest
+
+
 const Skills = () => {
   return <div>Skills</div>;
 };
