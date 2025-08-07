70ec732dc7f8e1654f3016060d900f2896371583
Cloudyday56 <Rchen78900@gmail.com>
2025-08-06 20:49:57 -0400

    Skills update - SWE section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -103,7 +103,7 @@ const Skills = () => {
                 <span className="text-gray-500 font-medium">Aseprite</span>
               </li>
               <li>
-                - 3D modeling {" "}
+                - 3D modeling{" "}
                 <span className="text-gray-500 font-medium">Blender</span>
               </li>
               <li>- Level design and playtesting</li>
@@ -132,11 +132,27 @@ const Skills = () => {
           <div className="flex flex-col gap-2">
             <h3 className="text-2xl font-bold text-secondary">SWE</h3>
             <p className="text-base text-gray-300">
-              I've developed several programming skills in-school as well as
-              through personal projects. Machine learning and AI has been an
-              ongoing interest of mine, and I've been learning related concepts
-              continuously.
+              I've built solid programming skills through both coursework and personal projects.
+              Machine learning and AI has been an ongoing interest of mine, and I've been learning
+              related concepts continuously. Related skills:
             </p>
+            <ul className="list-none text-base text-gray-300 pl-8 mb-2 mt-2">
+              <li>
+                - Object-oriented programming (
+                <span className="text-gray-500 font-medium">Java</span>,{" "}
+                <span className="text-gray-500 font-medium">Python</span>)
+              </li>
+              <li>
+                - Version control with{" "}
+                <span className="text-gray-500 font-medium">Git & GitHub</span>
+              </li>
+              <li>- Data structures & algorithms</li>
+              <li>
+                - Machine learning with{" "}
+                <span className="text-gray-500 font-medium">scikit-learn</span>,{" "}
+                <span className="text-gray-500 font-medium">random forest</span>
+              </li>
+            </ul>
             <div className="flex flex-row gap-4 mt-2">
               <img
                 src="https://skillicons.dev/icons?i=py,java,github,sklearn&theme=dark"
